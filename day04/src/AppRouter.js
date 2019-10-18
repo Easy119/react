f46c09e9c9da72f4daf3345d2d133bbef738f1c5
徐 <nndhysc@163.com>
2019-10-18 18:32:37 +0800

    a

--- day04/src/AppRouter.js
+++ day04/src/AppRouter.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Layout, Menu } from 'element-react';
+import 'element-theme-default';
 import './index.css'
 import Index from './pages/Index'
 import List from './pages/List'
@@ -23,6 +25,13 @@ export default class AppRouter extends Component {
             ]
         })
     }
+    onOpen() {
+
+    }
+
+    onClose() {
+
+    }
     render() {
         let routerList = this.state.routeConfig.map((item, index) => {
             return (
@@ -34,21 +43,11 @@ export default class AppRouter extends Component {
         return (
             <div>
                 <Router>
-                    {/* <ul>
-                        <li><Link to='/'>首页</Link></li>
-                        <li><Link to='/list/123'>列表</Link></li>
-                    </ul>
-                    <Route path='/' exact component={Index}></Route>
-                    <Route path="/home/" component={Home} />
-                    <Route path='/list/:id' exact component={List}></Route> */}
+
                     <div className="mainDiv">
-                        <div className="leftNav">
-                            <h3>一级导航</h3>
-                            <ul>{routerList}
-                                {/* <li><Link to="/">博客首页</Link> </li>
-                                <li><Link to="/video/">视频教程</Link> </li>
-                                <li><Link to="/workplace/">职场技能</Link> </li> */}
-                            </ul>
+                        {/* <div className="leftNav">
+                            <h3>易山川的作品集</h3>
+                            <ul>{routerList}</ul>
                         </div>
 
                         <div className="rightMain">
@@ -57,10 +56,43 @@ export default class AppRouter extends Component {
                                     return (<Route key={index} exact={item.exact} path={item.path} component={item.component} />)
                                 })
                             }
-                            {/* <Route path="/" exact component={Index} />
-                            <Route path="/video/" component={Video} />
-                            <Route path="/workplace/" component={Workplace} /> */}
-                        </div>
+                          
+                        </div> */}
+
+                        {/* element-ui */}
+                        <Layout.Row className="mainDiv">
+                            <Layout.Col span={2}>
+                                <Menu defaultActive="2" className="el-menu-vertical-demo" onOpen={this.onOpen.bind(this)} onClose={this.onClose.bind(this)} theme='dark'>
+                                    <Menu.SubMenu index="1" title={<span><i className="el-icon-message"></i>导航一</span>}>
+                                        <Menu.ItemGroup title="分组一">
+                                            <Menu.Item index="">选项1</Menu.Item>
+                                            <Menu.Item index="1-2">选项2</Menu.Item>
+                                        </Menu.ItemGroup>
+                                        <Menu.ItemGroup title="分组2">
+                                            <Menu.Item index="1-3">选项3</Menu.Item>
+                                        </Menu.ItemGroup>
+                                    </Menu.SubMenu>
+                                    {
+                                        this.state.routeConfig.map((item, index) => {
+                                            return (
+                                                <Menu.Item index={item.path + index} key={item.path + index}>
+                                                    <i className="el-icon-setting"></i>
+                                                    <Link to={item.path} exact={item.exact.toString()}>{item.title}</Link>
+                                                </Menu.Item>
+                                            )
+                                        })
+                                    }
+                                    {/* <Menu.Item index="3"><i className="el-icon-setting"></i>导航三</Menu.Item> */}
+                                </Menu>
+                            </Layout.Col>
+                            <Layout.Col span={22}>
+                                {
+                                    this.state.routeConfig.map((item, index) => {
+                                        return (<Route key={index} exact={item.exact} path={item.path} component={item.component} />)
+                                    })
+                                }
+                            </Layout.Col>
+                        </Layout.Row>
                     </div>
                 </Router>
             </div>
